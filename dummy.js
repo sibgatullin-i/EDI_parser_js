8547cebafcc3878aa7f541537027886e40bf3eeb
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2023-11-22 15:32:02 +0300

    add parseEDIFiles

--- dummy.js
+++ dummy.js
@@ -1,2 +1,12 @@
-import { parseEDIfile } from "./src/parse";
+import { parseEDIfile } from "./src/parse.js";
+import { fileURLToPath } from 'url';
+import * as path from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, 'sample', 'indexgso.htm')
+
+const result = parseEDIfile(filePath)
+
+console.log(result)
--- src/parse.js
+++ src/parse.js
@@ -1,10 +1,34 @@
 import fs from 'fs';
-import { parse } from 'node-html-parser';
+import * as cheerio from 'cheerio';
+import cheerioTableparser from 'cheerio-tableparser';
 
 export function parseEDIfile (filePath) {
   const rawHTML = fs.readFileSync(filePath, 'utf8');
-  const HTML = parse(rawHTML);
+  const HTML = cheerio.load(rawHTML);
+  const HTMLHeader = HTML('body > table > tbody > tr > td > p').contents()[1].data
+  const HTMLDate = HTMLHeader.substring(HTMLHeader.length - 10);
   
-
-
+  cheerioTableparser(HTML);
+  const dataRaw = HTML('body > table > tbody > tr > td> table > tbody').parsetable(false, false, false);
+  const data = [];
+  for (let i = 1; i < dataRaw[0].length; i++) {
+    data.push({
+      EventId: dataRaw[0][i].split('>')[1].replace('</a',''),
+      URL: dataRaw[0][i].split('"')[1],
+      //[dataRaw[0][0]]: dataRaw[0][i],
+      [dataRaw[1][0].replace('<b>','').replace('</b>','')]: dataRaw[1][i],
+      [dataRaw[2][0].replace('<b>','').replace('</b>','')]: dataRaw[2][i],
+      [dataRaw[3][0].replace('<b>','').replace('</b>','')]: dataRaw[3][i],
+      [dataRaw[4][0].replace('<b>','').replace('</b>','')]: dataRaw[4][i],
+      [dataRaw[5][0].replace('<b>','').replace('</b>','')]: dataRaw[5][i],
+      [dataRaw[6][0].replace('<b>','').replace('</b>','')]: dataRaw[6][i],
+      [dataRaw[7][0].replace('<b>','').replace('</b>','')]: dataRaw[7][i],
+      [dataRaw[8][0].replace('<b>','').replace('</b>','')]: dataRaw[8][i],
+    })
+  }
+  return {
+    HTMLHeader,
+    HTMLDate,
+    data
+  }
 }
